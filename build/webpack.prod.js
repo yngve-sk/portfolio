e0988681f3eb45682225596ce67188fa2cd872ae
Fabian Bolte <u.dod@gmx.de>
2020-08-14 10:28:42 +0200

    update packages

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -14,28 +14,30 @@ module.exports = merge(common, {
   mode: "production",
   plugins: [
     new CleanWebpackPlugin(),
-    new CopyWebpackPlugin([
-      {
-        from: resolve("assets/svg"),
-        to: resolve("dist/assets/svg"),
-        toType: "dir"
-      },
-      {
-        from: resolve("libs"),
-        to: resolve("dist/libs"),
-        toType: "dir"
-      },
-      {
-        from: resolve("deploy/README.md"),
-        to: resolve("dist/README.md"),
-        toType: "file"
-      },
-      {
-        from: resolve("deploy/CNAME"),
-        to: resolve("dist/CNAME"),
-        toType: "file"
-      }
-    ])
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: resolve("assets/svg"),
+          to: resolve("dist/assets/svg"),
+          toType: "dir"
+        },
+        {
+          from: resolve("libs"),
+          to: resolve("dist/libs"),
+          toType: "dir"
+        },
+        {
+          from: resolve("deploy/README.md"),
+          to: resolve("dist/README.md"),
+          toType: "file"
+        },
+        {
+          from: resolve("deploy/CNAME"),
+          to: resolve("dist/CNAME"),
+          toType: "file"
+        }
+      ]
+    })
   ],
   optimization: {
     splitChunks: {
