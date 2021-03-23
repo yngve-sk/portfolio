4bb8205fd767febce6b0a87c1fc82bd3b5587631
Fabian Bolte <u.dod@gmx.de>
2021-03-23 19:16:28 +0100

    adjust about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -11,23 +11,31 @@ export default {
       data: {
         title: "About",
         descriptionHTML: `
-        <p>Hello and thank you for visiting my website, where I showcase some of the projects I have been working on during my career and free time.</p>
         <p>
-          Computers were always around me. Pictures captured me sitting sitting in front of an Amiga when I was 1.5 years old  I played Lemmings, Monkey Island, and Giana Sisters in early years.
-          When I got a graphical calculator in 8th grade, I began programming it in Basic. With my passion for computer games, I started my Bachelor's degree in computer science with a specialization in computer graphics.
-          I enjoyed solving problems in all kinds of areas and my curiosity led me to study in a broad variety of fields, so that I wrote my thesis in artificial intelligence and started my Master's in high performance and cloud computing.
-          I then discovered visualization as a topic that combines both visual appeal and problem solving. It was not only interesting enough to write my Master's thesis in the field, but further
-          to pursue a PhD at the University of Bergen, Norway. There I developed multiple web-based applications and visualization methods
-          and performed research on the comparison of visualizations to find the best one for a given user, task, and dataset.
-          After finishing my PhD, I took a job at Magic Leap, where I now aim to combine my knowledge of visualizations with that of 3D spatial computing.
+        Hello and thank you for visiting my website, where I showcase some of the projects I have been working on during my career and free time.
+        I got a PhD in data visualization from the University of Bergen, Norway, and currently I work at Magic Leap,
+        where I aim to combine my knowledge of visualizations with that of 3D spatial computing.
         </p>
         <p>
-          I love playing drums and volleyball.
+          Computers were always around me. Pictures captured me sitting in front of an Amiga when I was 1.5 years old.
+          I played Lemmings, Monkey Island, and Giana Sisters in early years.
+          When I got a graphical calculator in 8th grade, I began programming it in Basic.
+          With my passion for computer games, I started my Bachelor's degree in computer science with a specialization in computer graphics.
+          I enjoyed solving problems in all kinds of areas and my curiosity led me to study in a broad variety of fields,
+          so that I wrote my thesis in artificial intelligence and started my Master's in high performance and cloud computing.
+          I then discovered visualization as a topic that combines both visual appeal and problem solving.
+          It was not only interesting enough to write my Master's thesis in the field, but further
+          to pursue a PhD at the University of Bergen, Norway.
+          There, I developed multiple web-based applications and visualization methods
+          and performed research on the comparison of visualizations to find the best one for a given user, task, and dataset.
+        </p>
+        <p>
+          I love to play drums and volleyball.
           I like being outdoors, viewing nature, hiking the mountains around Bergen, or lying at a beach looking at the ocean.
           I still love video games.
         </p>
         `,
-      image: require('../../assets/images/about_me.jpg')
+        image: require('../../assets/images/about_me.jpg')
       }
     };
   }
