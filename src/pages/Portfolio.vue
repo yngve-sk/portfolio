5cb283920979a38ccbc0a071bf88b8f5fd2c89c3
Yngve.S.Kristiansen <yngve.s.kristiansen@uib.no>
2022-08-25 13:05:58 +0200

    Added descs

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -27,90 +27,90 @@ export default {
           title: "Content Driven Layout",
           image: require("../../assets/images/mesh-painter.png"),
           link: "content-driven-layout",
-          description: "YK-project-2DO."
+          description: "Computer-vision based force layout."
         },
         {
           title: "Semantic Snapping",
-          image: require("../../assets/images/mesh-painter.png"),
+          image: require("../../assets/images2/semsnap-casestudy2.png"),
           link: "semantic-snapping",
-          description: "YK-project-2DO."
+          description: "Semi-automatic refinement of multi-view visualizations."
         },
         {
-          title: "Volume Renderer in the web browser",
+          title: "WebGL Volume Renderer",
           image: { src: "/assets/images2/webglvr_windowing-addsubview.gif" },
           link: "webgl-volume",
           description: "WebGL+Angular powered medical volume renderer"
         },
-        {
-          title: "D3 Window",
-          image: require("../../assets/images/mesh-painter.png"),
-          link: "d3-window",
-          description: "YK-project-2DO."
-        },
-        {
-          title: "Drags",
-          image: require("../../assets/images/mesh-painter.png"),
-          link: "drags",
-          description: "YK-project-2DO."
-        },
+        // {
+        //   title: "D3 Window",
+        //   image: require("../../assets/images/mesh-painter.png"),
+        //   link: "d3-window",
+        //   description: "YK-project-2DO."
+        // },
+        // {
+        //   title: "Drags",
+        //   image: require("../../assets/images/mesh-painter.png"),
+        //   link: "drags",
+        //   description: "YK-project-2DO."
+        // },
         {
           title: "Git-based Course AutoGrading System",
           image: require("../../assets/images2/gitlab-ci-pipelines.png"),
           link: "course-management-sys",
           description: "System for managing grading, students and TA's in programming courses."
         },
-        {
-          title: "Mesh Painter",
-          image: require("../../assets/images/mesh-painter.png"),
-          link: "meshpainter",
-          description: "3D mesh annotation via 2D brushing."
-        },
-        {
-          title: "Slot Machine",
-          image: require("../../assets/images/slots.png"),
-          link: "slots",
-          description: "A simple slot machine based on the Godot engine."
-        },
-        {
-          title: "Organic Narrative Charts",
-          image: require("../../assets/images/orcha.jpg"),
-          link: "orcha",
-          description:
-            "Digital recreation of Ward Shelley's diagrammatic paintings and prototype for an authoring tool."
-        },
-        {
-          title: "SplitStreams",
-          image: require("../../assets/images/streams.png"),
-          link: "splitstreams",
-          description:
-            "JavaScript library for the easy visualization of hierarchical changes and evolution of tree structures."
-        },
-        {
-          title: "Vis-a-Vis",
-          image: require("../../assets/images/source.png"),
-          link: "visavis",
-          description:
-            "Web-based IDE to support and enhance the development of prototypes for visualization algorithms."
-        },
-        {
-          title: "Hydroponics O.L.D.",
-          image: require("../../assets/images/OLD-interface1.png"),
-          link: "old",
-          description: `Self-regulating unit for automatic nutrition adjustment and watering of plants for optimal growth conditions.`
-        },
-        {
-          title: "Author Collaborations",
-          image: require("../../assets/images/streamgraph.png"),
-          link: "stream",
-          description:
-            "Visualization of collaboration efforts in university departments and their evolution over time."
-        },
-        {
-          title: "Macula Degeneration",
-          image: require("../../assets/images/macula.png"),
-          description:
-            "Collaboration with University Eye Clinic Greifswald to visualize age-related macula degeneration."
-        },
+        // {
+        //   title: "Mesh Painter",
+        //   image: require("../../assets/images/mesh-painter.png"),
+        //   link: "meshpainter",
+        //   description: "3D mesh annotation via 2D brushing."
+        // },
+        // {
+        //   title: "Slot Machine",
+        //   image: require("../../assets/images/slots.png"),
+        //   link: "slots",
+        //   description: "A simple slot machine based on the Godot engine."
+        // },
+        // {
+        //   title: "Organic Narrative Charts",
+        //   image: require("../../assets/images/orcha.jpg"),
+        //   link: "orcha",
+        //   description:
+        //     "Digital recreation of Ward Shelley's diagrammatic paintings and prototype for an authoring tool."
+        // },
+        // {
+        //   title: "SplitStreams",
+        //   image: require("../../assets/images/streams.png"),
+        //   link: "splitstreams",
+        //   description:
+        //     "JavaScript library for the easy visualization of hierarchical changes and evolution of tree structures."
+        // },
+        // {
+        //   title: "Vis-a-Vis",
+        //   image: require("../../assets/images/source.png"),
+        //   link: "visavis",
+        //   description:
+        //     "Web-based IDE to support and enhance the development of prototypes for visualization algorithms."
+        // },
+        // {
+        //   title: "Hydroponics O.L.D.",
+        //   image: require("../../assets/images/OLD-interface1.png"),
+        //   link: "old",
+        //   description: `Self-regulating unit for automatic nutrition adjustment and watering of plants for optimal growth conditions.`
+        // },
+        // {
+        //   title: "Author Collaborations",
+        //   image: require("../../assets/images/streamgraph.png"),
+        //   link: "stream",
+        //   description:
+        //     "Visualization of collaboration efforts in university departments and their evolution over time."
+        // },
+        // {
+        //   title: "Macula Degeneration",
+        //   image: require("../../assets/images/macula.png"),
+        //   description:
+        //     "Collaboration with University Eye Clinic Greifswald to visualize age-related macula degeneration."
+        // },
         // {
         //   title: "Customer Localization",
         //   image: require("../../assets/images/source.png"),
@@ -123,29 +123,29 @@ export default {
         //   link: "master",
         //   description: "possibly"
         // },
-        {
-          title: "Reinforcement Learning",
-          image: require("../../assets/images/bachelor.png"),
-          description:
-            "Implementation of a reinforcement learning agent to find an optimal sequence of actions in unknown environments."
-        },
+        // {
+        //   title: "Reinforcement Learning",
+        //   image: require("../../assets/images/bachelor.png"),
+        //   description:
+        //     "Implementation of a reinforcement learning agent to find an optimal sequence of actions in unknown environments."
+        // },
         // {
         //   title: "Collision Detection for Dynamic Meshes",
         //   image: require("../../assets/images/source.png"),
         //   link: "master",
         //   description: "possibly"
         // },
-        {
-          title: "Pong 2D/3D",
-          image: require("../../assets/images/pong.jpg"),
-          description: "Reimplementation of the OpenGL pipeline in software."
-        },
-        {
-          title: "Manta",
-          image: require("../../assets/images/Manta.jpg"),
-          description:
-            "Simple game for learning 3D transformations and lightning models."
-        }
+        // {
+        //   title: "Pong 2D/3D",
+        //   image: require("../../assets/images/pong.jpg"),
+        //   description: "Reimplementation of the OpenGL pipeline in software."
+        // },
+        // {
+        //   title: "Manta",
+        //   image: require("../../assets/images/Manta.jpg"),
+        //   description:
+        //     "Simple game for learning 3D transformations and lightning models."
+        // }
       ]
     };
   }
