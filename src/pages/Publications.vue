6a2623e00399fe0c33f404244508d6aff7b65d2d
Yngve.S.Kristiansen <yngve.s.kristiansen@uib.no>
2022-08-30 14:33:58 +0200

    Add publications

--- src/pages/Publications.vue
+++ src/pages/Publications.vue
@@ -16,106 +16,73 @@ export default {
   data() {
     return {
       meta: {
-        trautner2020sunspotplots: {
-          image: require('../../assets/images/sunspotGeo.png'),
-          video:
-            'https://vis.uib.no/wp-content/papercite-data/vids/Trautner_2020_SunspotPlots_video.mp4'
+        Kristiansen2022ContentDriven: {
+          image: { src: 'https://vis.uib.no/wp-content/papercite-data/images/Kristiansen-2022-LungsDt.PNG' },
         },
-        bolte2020orcha: {
-          image: require('../../assets/images/orcha.jpg')
+        'Kristiansen-2021-SSG': {
+          image: { src: 'https://vis.uib.no/wp-content/papercite-data/images/Kristiansen-2021-SSG.png' },
+          video: 'https://vis.uib.no/wp-content/papercite-data/vids/Kristiansen-2021-SSG.mp4'
         },
-        bolte2020splitstreams: {
-          image: require('../../assets/images/streams.png')
+        'Kristiansen-2020-VIV': {
+          image: {src: 'https://vis.uib.no/wp-content/papercite-data/images/Kristiansen-2020-VIV.jpg' },
+          video: 'https://vis.uib.no/wp-content/papercite-data/vids/Trautner_2020_SunspotPlots_video.mp4'
         },
-        bolte2020visavis: {
-          image: require('../../assets/images/source.png'),
-          video: 'https://youtu.be/5XO6BU4j1KQ'
-        },
-        bolte2020MVS: {
-          image: require('../../assets/images/measures.png'),
-          abstract:
-            'Measurement is an integral part of modern science, providing the fundamental means for evaluation, comparison, and prediction. In the context of visualization, several different types of measures have been proposed, ranging from approaches that evaluate particular aspects of individual visualization techniques, their perceptual characteristics, and even economic factors. Furthermore, there are approaches that attempt to provide means for measuring general properties of the visualization process as a whole. Measures can be quantitative or qualitative, and one of the primary goals is to provide objective means for reasoning about visualizations and their effectiveness. As such, they play a central role in the development of scientific theories for visualization. In this chapter, we provide an overview of the current state of the art, survey and classify different types of visualization measures, characterize their strengths and drawbacks, and provide an outline of open challenges for future research.'
-        },
-        ivapp19: {
-          image: require('../../assets/images/streamgraph.png')
-        }
       },
       bibtexParsed: {},
       bibtex: `
-      @article {trautner2020sunspotplots,
-        journal = {Computer Graphics Forum},
-        title = {{Sunspot Plots}: {M}odel-based Structure Enhancement for Dense Scatter Plots},
-        author = {Trautner, Thomas and Bolte, Fabian and Stoppel, Sergej and Bruckner, Stefan},
-        year = {2020},
-        volume = {39},
-        number = {3},
-        pages = {},
-        publisher = {The Eurographics Association and John Wiley & Sons Ltd.},
-        keywords = {Human-centered computing, Visualization techniques, Information visualization, Empirical studies in visualization},
-        ISSN = {1467-8659},
-        DOI = {10.1111/cgf.14001},
-        url = {https://vis.uib.no/wp-content/papercite-data/pdfs/Trautner_2020_SunspotPlots_PDF.pdf}
-      },
+@article{Kristiansen2022ContentDriven,
+	title        = {Content-Driven Layout for Visualization Design},
+	author       = {Kristiansen, Y. S. and Garrison, L. and Bruckner, S.},
+	year         = 2022,
+	journal      = {Proceedings of the International Symposium on Visual Information Communication and Interaction (to appear)},
+	volume       = {},
+	pages        = {},
+	doi          = {},
+	issn         = {},
+	url          = {https://vis.uib.no/wp-content/papercite-data/pdfs/Kristiansen-2022-CDL.pdf},
+    abstract     = {Multi-view visualizations are typically presented in a grid layout with elements positioned according to their bounding rectangles. These rectangles often contain unused white space. In cases where Tufte’s Shrink Principle can be applied to reduce non-data-ink without impairing the communication of information, unused white space can be utilized for the placement of other elements. This is often done in manually “hand-crafted” layouts by designers. However, upon changes to individual elements, this design process has to be repeated. To reduce non-data-ink and repetitive manual design, we contribute a method for automatically turning a grid layout into a content-driven layout, where elements are positioned with respect to their contents. Existing approaches have explored the use of a force simulation in conjunction with proxy geometries to simplify collision handling for irregular shapes. Such customized force directed layouts are usually unstable, and often require additional constraints to run properly. In addition, proxy geometries become less accurate and effective with more irregular shapes. To solve these shortcomings, we contribute an approach for identifying central elements in an original grid layout in order to set up corresponding attractive forces. Furthermore, we utilize an imagebased approach for collision detection and avoidance that works accurately for highly irregular shapes. We demonstrate the utility of our approach with three case studies.},
+    images     = "images/Kristiansen-2022-LungsDt.PNG",
+    thumbnails = "images/Kristiansen-2022-LungsDt.PNG",
+    pdf = {pdfs/Kristiansen-2022-CDL.pdf},
+    project = "MetaVis",
+}
 
-      @inproceedings {bolte2020orcha,
-        booktitle = {Eurographics 2020 - Short Papers},
-        title = {{Organic Narrative Charts}},
-        author = {Bolte, Fabian and Bruckner, Stefan},
-        year = {2020},
-        publisher = {The Eurographics Association},
-        ISSN = {1017-4656},
-        ISBN = {978-3-03868-101-4},
-        DOI = {10.2312/egs.20201026},
-        url={https://arxiv.org/pdf/2004.13896.pdf}
-      },
-      
-      @ARTICLE{bolte2020splitstreams,
-        author={Bolte, Fabian and Nourani, Mahsan and Ragan, Eric D. and Bruckner, Stefan},
-        journal={IEEE Transactions on Visualization and Computer Graphics},
-        title={{SplitStreams}: {A} Visual Metaphor for Evolving Hierarchies},
-        year={2020},
-        doi={10.1109/TVCG.2020.2973564},
-        ISSN={1077-2626},
-        url={https://arxiv.org/pdf/2002.03891.pdf},
-        keywords={Visualization, hierarchy data, time-varying data, streamgraphs, treemaps.}
-      },
-
-      @ARTICLE{bolte2020visavis,
-        author={Bolte, Fabian and Bruckner, Stefan},
-        journal={IEEE Transactions on Visualization and Computer Graphics},
-        title={{Vis-a-Vis}: {V}isual Exploration of Visualization Source Code Evolution},
-        year={2020},
-        keywords={Visualization System and Toolkit Design;User Interfaces;Integrating Spatial and Non-Spatial Data Visualization;Software Visualization},
-        doi={10.1109/TVCG.2019.2963651},
-        ISSN={2160-9306},
-        url = {https://arxiv.org/pdf/2001.02092.pdf}
-      },
-
-      @incollection {bolte2020MVS,
-        author = {Bolte, Fabian and Bruckner, Stefan},
-        title = {Measures in Visualization Space},
-        booktitle = {Foundations of Data Visualization},
-        chapter = {3},
-        publisher = {Springer},
-        year = {2020},
-        note = {This is a preprint of a chapter for a planned book that was initiated by participants of the Dagstuhl Seminar 18041 ("Foundations of Data Visualization") and that is expected to be published by Springer. The final book chapter will differ from this preprint.},
-        isbn = {978-3-030-34444-3},
-        doi = {10.1007/978-3-030-34444-3},
-        url = {https://arxiv.org/pdf/1909.05295}
-      }
-
-      @inproceedings{ivapp19,
-        author={Rosenthal, Paul and Müller, Nicholas H. and Bolte, Fabian},
-        title={Visual Analytics of Bibliographical Data for Strategic Decision Support of University Leaders: {A} Design Study},
-        booktitle={Proceedings of the 14th International Joint Conference on Computer Vision, Imaging and Computer Graphics Theory and Applications - Volume 3: IVAPP},
-        year={2019},
-        pages={297-305},
-        publisher={SciTePress},
-        organization={INSTICC},
-        doi={10.5220/0007396302970305},
-        isbn={978-989-758-354-4},
-        url = {https://www.paul-rosenthal.de/wp-content/uploads/2019/03/rosenthal_ivapp_2019.pdf}
-      }`
+@Article{Kristiansen-2021-SSG,
+  author     = {Kristiansen, Y. S. and Garrison, L. and Bruckner, S.},
+  title      = {Semantic Snapping for Guided Multi-View Visualization Design},
+  journal    = {IEEE Transactions on Visualization and Computer Graphics},
+  year       = {2021},
+  volume     = {},
+  pages      = {},
+  doi        = {},
+  abstract   = {Visual information displays are typically composed of multiple visualizations that are used to facilitate an understanding of the underlying data. A common example are dashboards, which are frequently used in domains such as finance, process monitoring and business intelligence. However, users may not be aware of existing guidelines and lack expert design knowledge when composing such multi-view visualizations. In this paper, we present semantic snapping, an approach to help non-expert users design effective multi-view visualizations from sets of pre-existing views. When a particular view is placed on a canvas, it is “aligned” with the remaining views–not with respect to its geometric layout, but based on aspects of the visual encoding itself, such as how data dimensions are mapped to channels. Our method uses an on-the-fly procedure to detect and suggest resolutions for conflicting, misleading, or ambiguous designs, as well as to provide suggestions for alternative presentations. With this approach, users can be guided to avoid common pitfalls encountered when composing visualizations. Our provided examples and case studies demonstrate the usefulness and validity of our approach.},
+  note       = {Accepted for publication, to be presented at IEEE VIS 2021},
+  project    = {MetaVis,VIDI},
+  url       = {https://vis.uib.no/wp-content/papercite-data/pdfs/Kristiansen-2021-SSG.pdf},
+  pdf       = {pdfs/Kristiansen-2021-SSG.pdf},
+  vid       = {https://vis.uib.no/wp-content/papercite-data/vids/Kristiansen-2021-SSG.mp4},
+  thumbnails = {images/Kristiansen-2021-SSG.png},
+  images    = {images/Kristiansen-2021-SSG.jpg},
+  keywords  = {tabular data, guidelines, mixed initiative human-machine analysis, coordinated and multiple views},
+  doi = {10.1109/TVCG.2021.3114860},
+}
+@article{Kristiansen-2020-VIV,
+  author = {Kristiansen, Y. S. and Bruckner, S.},
+  title = {Visception: An Interactive Visual Framework for Nested Visualization Design},
+  journal = {Computers \& Graphics},
+  volume = {92},
+  pages = {13--27},
+  keywords = {information visualization, nested visualizations, nesting},
+  doi = {10.1016/j.cag.2020.08.007},
+  abstract = {Nesting is the embedding of charts into the marks of another chart. Related to principles such as Tufteâs rule of utilizing micro/macro readings, nested visualizations have been employed to increase information density, providing compact representations of multi-dimensional and multi-typed data entities. Visual authoring tools are becoming increasingly prevalent, as they make visualization technology accessible to non-expert users such as data journalists, but existing frameworks provide no or only very limited functionality related to the creation of nested visualizations. In this paper, we present an interactive visual approach for the flexible generation of nested multilayer visualizations. Based on a hierarchical representation of nesting relationships coupled with a highly customizable mechanism for specifying data mappings, we contribute a flexible framework that enables defining and editing data-driven multi-level visualizations. As a demonstration of the viability of our framework, we contribute a visual builder for exploring, customizing and switching between different designs, along with example visualizations to demonstrate the range of expression. The resulting system allows for the generation of complex nested charts with a high degree of flexibility and fluidity using a drag and drop interface.},
+  year = {2020},
+  url = {https://vis.uib.no/wp-content/papercite-data/pdfs/Kristiansen-2020-VIV.pdf},
+  pdf = "pdfs/Kristiansen-2020-VIV.pdf",
+  thumbnails = "images/Kristiansen-2020-VIV.png",
+  images = "images/Kristiansen-2020-VIV.jpg",
+  project = "MetaVis"
+}
+`
     };
   },
   created() {
