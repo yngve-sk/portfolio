f421dee20ecd1a9a5a1517f4e7e278148ed8de03
Yngve.S.Kristiansen <yngve.s.kristiansen@uib.no>
2022-09-05 00:12:18 +0200

    Add own info and push to main instead of master

--- deploy/deploy.bat
+++ deploy/deploy.bat
@@ -2,7 +2,7 @@ cd ../dist
 git init
 git add .
 git commit -m "update"
-git remote add origin https://github.com/CadanoX/cadanox.github.io.git
-git push -u origin master --force
+git remote add origin https://github.com/yngve-sk/yngve-sk.github.io.git
+git push -u origin main --force
 cd ..
 @pause
